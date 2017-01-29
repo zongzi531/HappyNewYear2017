4fa546cf26ee315c0d3a2c3f3592541453cacbd7
zongzi531 <1668259989@qq.com>
2017-01-30 00:24:54 +0800

    v.0.5
    1.加入载入画面
    2.重新coding硬滑动效果（不是很理想，存在bug）
    3.增加p1新CSS动画

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 /*  init  */
+var pageloading = document.getElementById("loading_page");
 var page1 = document.getElementById("page1");
 var page2 = document.getElementById("page2");
 var page3 = document.getElementById("page3");
 var bgmusic = document.getElementById("music");
 var bgaudio = document.getElementsByTagName("audio")[0];
-var touch_y_before,touch_y_after,lens;
+var touch_y_before,
+	touch_y_after,
+	lens; 
 
+
+/*  loadingpage 计时器  */
+setTimeout(function(){
+	pageloading.setAttribute("class", "page loading_done");
+	pageloading.setAttribute("class", "page hide");
+	page1.setAttribute("class", "page show");
+},2000);
 
 /*  music  */
  document.addEventListener("WeixinJSBridgeReady", function () { 
-       bgaudio.play();
+       setTimeout(function(){
+			pageloading.setAttribute("class", "page loading_done");
+			pageloading.setAttribute("class", "page hide");
+			page1.setAttribute("class", "page show");
+			bgaudio.play();
+		},2000);
    }, false); 
 
 bgmusic.addEventListener("touchstart",function() {
@@ -22,6 +37,12 @@ bgmusic.addEventListener("touchstart",function() {
 	};
 },false);
 
+function page_hide_show_tool (x,y) { /* 滑动页面延时控制器 */
+	setTimeout(function(){
+		x.setAttribute("class","page show");
+		y.setAttribute("class","page hide");
+	},500);
+}
 
 /*  page1  */
 page1.addEventListener("touchstart",function(event){
@@ -34,8 +55,9 @@ page1.addEventListener("touchend",function(event){
 	touch_y_after = touch.clientY;
 	lens = parseInt(touch_y_before - touch_y_after);
 	if (lens > 0) {
-		page1.style.display = "none";
-		page2.style.display = "block";
+		page1.setAttribute("class","page page_drop_down_this");
+		page2.setAttribute("class","page page_drop_down");
+		page_hide_show_tool(page2,page1);
 	};
 },false);
 
@@ -51,11 +73,13 @@ page2.addEventListener("touchend",function(event){
 	touch_y_after = touch.clientY;
 	lens = parseInt(touch_y_before - touch_y_after);
 	if (lens > 0) {
-		page2.style.display = "none";
-		page3.style.display = "block";
+		page2.setAttribute("class","page page_drop_down_this");
+		page3.setAttribute("class","page page_drop_down");
+		page_hide_show_tool(page3,page2);
 	} else {
-		page2.style.display = "none";
-		page1.style.display = "block";
+		page2.setAttribute("class","page page_drop_up_this");
+		page1.setAttribute("class","page page_drop_up");
+		page_hide_show_tool(page1,page2);
 	};
 },false);
 
@@ -71,7 +95,8 @@ page3.addEventListener("touchend",function(event){
 	touch_y_after = touch.clientY;
 	lens = parseInt(touch_y_after - touch_y_before);
 	if (lens > 0) {
-		page3.style.display = "none";
-		page2.style.display = "block";
+		page3.setAttribute("class","page page_drop_up_this");
+		page2.setAttribute("class","page page_drop_up");
+		page_hide_show_tool(page2,page3);
 	};
 },false);
